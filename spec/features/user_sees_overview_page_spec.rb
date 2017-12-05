80b54a36f11ca798b997cf120112c291fa28cd2e
BLaurenB <blbillington1@gmail.com>
2017-12-05 14:49:18 -0700

    updated overview page test so that it passes

--- spec/features/user_sees_overview_page_spec.rb
+++ spec/features/user_sees_overview_page_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-scenario "When user visits FakeHub root" do
-  describe "they can log in and see their overview page" do
+feature "When user visits FakeHub root" do
+  scenario "they can log in and see their overview page" do
 
     user = create(:user, token: ENV["GITHUB_USER_TOKEN"])
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)

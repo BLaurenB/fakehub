a715e948d369dd4c0b1227a1845110f68148134b
BLaurenB <blbillington1@gmail.com>
2017-12-05 21:51:37 -0700

    got containers working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user
+  helper_method :current_user, :hub_user
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def hub_user
+    connection = Faraday.new(url: "https://api.github.com/users/BLaurenB")
+    response = connection.get do |req|
+      req.headers['token'] = current_user.token
+    end
+    user_data = JSON.parse(response.body, symbolize_names: true)
+    @hub_user = HubUser.new(user_data)
+  end
 
 end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,17 +1,12 @@
 class DashboardController < ApplicationController
 
   def index
-
     connection = Faraday.new(url: "https://api.github.com/users/BLaurenB")
-
     response = connection.get do |req|
-      req.headers['token'] = current_user.token 
+      req.headers['token'] = current_user.token
     end
-
     user_data = JSON.parse(response.body, symbolize_names: true)
-
     @hub_user = HubUser.new(user_data)
-
   end
 
 end

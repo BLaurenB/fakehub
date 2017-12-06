d6f2e8a1cae2e4def9214f50c67cc82894fb8747
BLaurenB <blbillington1@gmail.com>
2017-12-05 18:15:11 -0700

    changed api call to use current user's token

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,13 +5,12 @@ class DashboardController < ApplicationController
     connection = Faraday.new(url: "https://api.github.com/users/BLaurenB")
 
     response = connection.get do |req|
-      req.headers['token'] = ENV["GITHUB_USER_TOKEN"]
+      req.headers['token'] = current_user.token 
     end
 
     user_data = JSON.parse(response.body, symbolize_names: true)
 
     @hub_user = HubUser.new(user_data)
-    # binding.pry
 
   end
 

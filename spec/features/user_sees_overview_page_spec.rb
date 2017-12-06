b68485480141633ea1df015f0a390ed537491561
BLaurenB <blbillington1@gmail.com>
2017-12-05 18:30:27 -0700

    tests working, but they are brittle

--- spec/features/user_sees_overview_page_spec.rb
+++ spec/features/user_sees_overview_page_spec.rb
@@ -6,21 +6,23 @@ feature "When user visits FakeHub root" do
     user = create(:user, token: ENV["GITHUB_USER_TOKEN"])
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
+    user_data = {
+                name: "BLaurenB",
+                image: "https://avatars0.githubusercontent.com/u/20262199?v=4",
+                public_repos: 38,
+                followers: 0,
+                following: 1
+              }
+
+    hub_user = HubUser.new(user_data)
+
     visit '/dashboard'
 
-    var = JSON.parse(response.body) #this is if I'm getting JSON back. WHat I want is to get JSON and then display the JSON, so I need to do something like current_user. but it current_user the db or the session, or all? do I need to have 2 separate variables for a logged-in user? Think if some other person was logging into my clone.
-
-
-    expect(page).to have_content(user.username)
-    expect(page).to have_content(user.image)
-    expect(page).to have_content(user.fullname)
-    expect(page).to have_content(user.repos)
-    expect(page).to have_content(user.stars)
-    expect(page).to have_content(user.followers)
-    expect(page).to have_content(user.followed)
-    expect(page).to have_content(user.bio)
-    expect(page).to have_content(user.location)
-    expect(page).to have_content(user.email)
+    expect(page).to have_content(hub_user.name)
+    expect(page).to have_content(hub_user.image)
+    expect(page).to have_content(hub_user.public_repos)
+    expect(page).to have_content(hub_user.followers)
+    expect(page).to have_content(hub_user.following)
 
 
   end

8a38974c4eeff0beee8109d01e34789d3ae58643
BLaurenB <blbillington1@gmail.com>
2018-01-31 21:01:55 -0700

    adding to tests

--- spec/features/user_can_login_spec.rb
+++ spec/features/user_can_login_spec.rb
@@ -3,26 +3,27 @@ require 'rails_helper'
 feature "Unauthenticated user can login with Github" do
   before do
     stub_omniauth
-  end 
+  end
 
   scenario  "using a valid GitHub account" do
     VCR.use_cassette("user_views_overview_page") do
       visit '/'
       click_on "Login with Github!"
       expect(current_path).to eq("/#{User.last.username}")
-    end 
-  end 
+    end
+  end
 
-  # it "they can see their overview page" do
-  #   VCR.use_cassette("user_views_overview_page") do
-    
-  #     expect(page).to_not have_content("Login with Github!")
-                
-  #     expect(page).to have_content("BlaurenB")
-  #     expect(page).to have_content("Repositories:28")
-  #     expect(page).to have_content("Following:1")
-    
-  #   end 
-  # end 
+    it "they can see their overview page" do
+      VCR.use_cassette("user_views_overview_page") do
+
+        visit '/'
+        click_on "Login with Github!"
+        expect(page).to_not have_content("Login with Github!")
+        expect(page).to have_content("BLaurenB")
+        expect(page).to have_content("Repositories:28")
+        expect(page).to have_content("Following:1")
+
+      end
+    end
 
 end
--- spec/models/api_data_spec.rb
+++ spec/models/api_data_spec.rb
@@ -1,18 +1,43 @@
 require 'rails_helper'
 
 describe ApiData do
-    it "exists" do
+  repo_data ={
+              name: "Bob",
+              image: "img link",
+              company: "",
+              location: "Denver",
+              bio: "I'm a Dev!",
+              public_repos: "72",
+              public_gists: "5",
+              followers: "34",
+              following: "50"}
 
+  let (:api_data) {ApiData.new(repo_data)}
+
+
+    it "exists" do
+      expect(api_data).to be_a ApiData
+    end
+
+    it "wraps a hash of API data in a class instance" do
+      expect(api_data.name).to eq("Bob")
+      expect(api_data.company).to eq("")
+      expect(api_data.location).to eq("Denver")
+      expect(api_data.bio).to eq("I'm a Dev!")
+      expect(api_data.public_repos).to eq("72")
+      expect(api_data.public_gists).to eq("5")
+      expect(api_data.followers).to eq("34")
+      expect(api_data.following).to eq("50")
+
+    end
         # User.new(uid: "1234567", username: "Joe", token: "token1234")
         # stub_omniauth
 
         # member = User.first
-        # expect(member).to be_a User 
-        # expect(member.uid).to eq("1234567") 
-        # expect(member.username).to eq("Joe") 
-        # expect(member.token).to eq("token1234") 
-    end
+        # expect(member).to be_a User
+        # expect(member.uid).to eq("1234567")
+        # expect(member.username).to eq("Joe")
+        # expect(member.token).to eq("token1234")
+
 
 end
-
-
